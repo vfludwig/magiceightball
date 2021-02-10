34c83662a0eecbb6960463f99ddfec62131c590b
Victoria Ludwig <vfludwig@mail.lipscomb.edu>
2021-02-10 12:28:27 -0600

    Update form_submit.php

--- form_submit.php
+++ form_submit.php
@@ -11,21 +11,22 @@ $connection->exec("set names utf8");
 echo "Connection error: " . $exception->getMessage();
 }
 
-function saveData($name, $email, $message){
+function saveData($question, $answer, $timedaterecorded){
 global $connection;
-$query = "INSERT INTO test(name, email, message) VALUES( :name, :email, :message)";
+$query = "INSERT INTO eightball(question, answer, timedaterecorded) VALUES( :question, :answer, :timedaterecorded)";
 
 $callToDb = $connection->prepare( $query );
-$name=htmlspecialchars(strip_tags($name));
-$email=htmlspecialchars(strip_tags($email));
-$message=htmlspecialchars(strip_tags($message));
-$callToDb->bindParam(":name",$name);
-$callToDb->bindParam(":email",$email);
-$callToDb->bindParam(":message",$message);
+$question=htmlspecialchars(strip_tags($question));
+$answer=htmlspecialchars(strip_tags($answer));
+$timedaterecorded=htmlspecialchars(strip_tags($timedaterecorded));
+$callToDb->bindParam(":question",$question);
+$callToDb->bindParam(":answer",$answer);
+$callToDb->bindParam(":timedaterecorded",$timedaterecorded);
 
+/*
 if($callToDb->execute()){
 return '<h3 style="text-align:center;">We will get back to you very shortly!</h3>';
-}
+}*/
 }
 
 if( isset($_POST['submit'])){
